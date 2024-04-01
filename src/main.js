491a40706daacdbeb9e42d5201242a147b33c023
Dima Kuznecov <kuznecov.dima2019@gmail.com>
2024-04-01 13:31:36 +0300

    git commit

--- src/main.js
+++ src/main.js
@@ -1,20 +1,8 @@
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-
-// import { createRouter, createWebHistory } from 'vue-router';
-
-// import Products from './components/Products/Products.vue'
 import App from './App.vue'
 import router from './router'
 import './styles/main.css'
-// const router = createRouter ({
-//   history: createWebHistory(),
-//   routes: [{
-//     name: 'Home',
-//     path: '/',
-//     component: 'Home'
-//   }]
-// });
 
 const app = createApp(App)
 
