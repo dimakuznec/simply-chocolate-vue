ed0fdc53da32b332f02f396b0e23b93a8de41f3d
Dima Kuznecov <kuznecov.dima2019@gmail.com>
2024-04-01 15:21:47 +0300

    git commit

--- src/components/Products/Products.vue
+++ src/components/Products/Products.vue
@@ -32,7 +32,11 @@ import '../Products/Products.css'
 									<br />
 									<span class="puddle-bg__products">Dark chocolate</span>
 									<br />
-									<span class="puddle-bg__products">45 UAH</span>
+									<button
+										class="puddle-bg__products puddle-bg__products__button"
+									>
+										45 UAH
+									</button>
 								</h3>
 							</div>
 						</article>
@@ -56,7 +60,11 @@ import '../Products/Products.css'
 										>Milk chocolate</span
 									>
 									<br />
-									<span class="puddle-bg__products">50 UAH</span>
+									<button
+										class="puddle-bg__products puddle-bg__products__button"
+									>
+										50 UAH
+									</button>
 								</h3>
 							</div>
 						</article>
@@ -86,7 +94,11 @@ import '../Products/Products.css'
 										>Dark chocolate</span
 									>
 									<br />
-									<span class="puddle-bg__products">66 UAH</span>
+									<button
+										class="puddle-bg__products puddle-bg__products__button"
+									>
+										66 UAH
+									</button>
 								</h3>
 							</div>
 						</article>
@@ -109,7 +121,11 @@ import '../Products/Products.css'
 									<br />
 									<span class="puddle-bg__products">Dark chocolate</span>
 									<br />
-									<span class="puddle-bg__products">54 UAH</span>
+									<button
+										class="puddle-bg__products puddle-bg__products__button"
+									>
+										54 UAH
+									</button>
 								</h3>
 							</div>
 						</article>
@@ -132,7 +148,11 @@ import '../Products/Products.css'
 									<br />
 									<span class="puddle-bg__products">Milk chocolate</span>
 									<br />
-									<span class="puddle-bg__products">45 UAH</span>
+									<button
+										class="puddle-bg__products puddle-bg__products__button"
+									>
+										45 UAH
+									</button>
 								</h3>
 							</div>
 						</article>
@@ -155,7 +175,11 @@ import '../Products/Products.css'
 									<br />
 									<span class="puddle-bg__products">Milk chocolate</span>
 									<br />
-									<span class="puddle-bg__products">50 UAH</span>
+									<button
+										class="puddle-bg__products puddle-bg__products__button"
+									>
+										50 UAH
+									</button>
 								</h3>
 							</div>
 						</article>
@@ -178,7 +202,11 @@ import '../Products/Products.css'
 									<br />
 									<span class="puddle-bg__products">Dark chocolate</span>
 									<br />
-									<span class="puddle-bg__products">66 UAH</span>
+									<button
+										class="puddle-bg__products puddle-bg__products__button"
+									>
+										66 UAH
+									</button>
 								</h3>
 							</div>
 						</article>
@@ -201,137 +229,16 @@ import '../Products/Products.css'
 									<br />
 									<span class="puddle-bg__products">White chocolate</span>
 									<br />
-									<span class="puddle-bg__products">54 UAH</span>
+									<button
+										class="puddle-bg__products puddle-bg__products__button"
+									>
+										54 UAH
+									</button>
 								</h3>
 							</div>
 						</article>
 					</li>
 				</ul>
-
-				<!-- <div class="reviews__actions">
-					<button class="reviews__arrow-button arrow-left" type="button">
-						<span class="visually-hidden">Previous slide</span>
-						<svg
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M22 13.5C22.8284 13.5 23.5 12.8284 23.5 12C23.5 11.1716 22.8284 10.5 22 10.5L22 13.5ZM0.939341 10.9393C0.353554 11.5251 0.353554 12.4749 0.93934 13.0607L10.4853 22.6066C11.0711 23.1924 12.0208 23.1924 12.6066 22.6066C13.1924 22.0208 13.1924 21.0711 12.6066 20.4853L4.12132 12L12.6066 3.51472C13.1924 2.92893 13.1924 1.97918 12.6066 1.3934C12.0208 0.807611 11.0711 0.807611 10.4853 1.3934L0.939341 10.9393ZM22 10.5L2 10.5L2 13.5L22 13.5L22 10.5Z"
-								fill="white"
-								fill-opacity="0.3"
-							/>
-						</svg>
-					</button>
-
-					<div class="reviews__pagination pagination">
-						<ul class="pagination__list">
-							<li class="pagination__item">
-								<button class="pagination__button is-current" type="button">
-									<span class="visually-hidden">First slide</span>
-									<svg
-										width="14"
-										height="14"
-										viewBox="0 0 14 14"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											d="M7.0099 2.05941L14 0L11.9604 7.0099L14 14L7.0099 11.9604L0 14L2.05941 7.0099L0 0L7.0099 2.05941Z"
-											fill="white"
-										/>
-									</svg>
-								</button>
-							</li>
-							<li class="pagination__item">
-								<button class="pagination__button" type="button">
-									<span class="visually-hidden">Second slide</span>
-									<svg
-										width="14"
-										height="14"
-										viewBox="0 0 14 14"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											d="M7.0099 2.05941L14 0L11.9604 7.0099L14 14L7.0099 11.9604L0 14L2.05941 7.0099L0 0L7.0099 2.05941Z"
-											fill="white"
-										/>
-									</svg>
-								</button>
-							</li>
-							<li class="pagination__item">
-								<button class="pagination__button" type="button">
-									<span class="visually-hidden">Third slide</span>
-									<svg
-										width="14"
-										height="14"
-										viewBox="0 0 14 14"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											d="M7.0099 2.05941L14 0L11.9604 7.0099L14 14L7.0099 11.9604L0 14L2.05941 7.0099L0 0L7.0099 2.05941Z"
-											fill="white"
-										/>
-									</svg>
-								</button>
-							</li>
-							<li class="pagination__item">
-								<button class="pagination__button" type="button">
-									<span class="visually-hidden">Fouth slide</span>
-									<svg
-										width="14"
-										height="14"
-										viewBox="0 0 14 14"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											d="M7.0099 2.05941L14 0L11.9604 7.0099L14 14L7.0099 11.9604L0 14L2.05941 7.0099L0 0L7.0099 2.05941Z"
-											fill="white"
-										/>
-									</svg>
-								</button>
-							</li>
-							<li class="pagination__item">
-								<button class="pagination__button" type="button">
-									<span class="visually-hidden">Fifth slide</span>
-									<svg
-										width="14"
-										height="14"
-										viewBox="0 0 14 14"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											d="M7.0099 2.05941L14 0L11.9604 7.0099L14 14L7.0099 11.9604L0 14L2.05941 7.0099L0 0L7.0099 2.05941Z"
-											fill="white"
-										/>
-									</svg>
-								</button>
-							</li>
-						</ul>
-					</div>
-
-					<button class="reviews__arrow-button arrow-right" type="button">
-						<span class="visually-hidden">Next slide</span>
-						<svg
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M2 10.5C1.17157 10.5 0.5 11.1716 0.5 12C0.5 12.8284 1.17157 13.5 2 13.5L2 10.5ZM23.0607 13.0607C23.6464 12.4749 23.6464 11.5251 23.0607 10.9393L13.5147 1.3934C12.9289 0.807613 11.9792 0.807613 11.3934 1.3934C10.8076 1.97919 10.8076 2.92893 11.3934 3.51472L19.8787 12L11.3934 20.4853C10.8076 21.0711 10.8076 22.0208 11.3934 22.6066C11.9792 23.1924 12.9289 23.1924 13.5147 22.6066L23.0607 13.0607ZM2 13.5L22 13.5L22 10.5L2 10.5L2 13.5Z"
-								fill="white"
-							/>
-						</svg>
-					</button>
-				</div> -->
 			</div>
 		</div>
 	</section>
