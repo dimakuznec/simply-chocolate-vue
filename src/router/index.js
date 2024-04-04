cc1c62c632af4d03a0f5e3d2c9762c2cb7867ede
Dima Kuznecov <kuznecov.dima2019@gmail.com>
2024-04-04 17:07:43 +0300

    git commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import header from '../views/header.vue';
+import header from '../views/header.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: 'header'
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/header.vue')
-    }
-  ]
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: 'header',
+		},
+		{
+			path: '/about',
+			name: 'about',
+			// route level code-splitting
+			// this generates a separate chunk (About.[hash].js) for this route
+			// which is lazy-loaded when the route is visited.
+			component: () => import('../views/header.vue'),
+		},
+	],
 })
 
 export default router
